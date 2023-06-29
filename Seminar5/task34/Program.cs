e5ef0cbe8649ddc27f434e2aba47b809fa278b67
ALEXANDRA LUCHKINA <dginerva@mail.ru>
2023-06-29 18:53:56 +0300

    завершила домашнюю работу по 5 семинару с#

---
+++ Seminar5/task34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+{
+Random random = new Random();
+
+int length = 10;
+int[] numbers = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+numbers[i] = random.Next(100, 1000);
+}
+
+int countEven = 0;
+for (int i = 0; i < length; i++)
+{
+if (numbers[i] % 2 == 0)
+{
+countEven++;
+}
+}
+
+Console.WriteLine("Сгенерированный массив:");
+for (int i = 0; i < length; i++)
+{
+Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\nКоличество четных чисел: " + countEven);
+
+Console.ReadLine();
+}
---
+++ Seminar5/task36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+{
+Random random = new Random();
+
+// Задаем размерность массива
+int length = 10;
+int[] numbers = new int[length];
+
+// Заполняем массив случайными числами
+for (int i = 0; i < length; i++)
+{
+numbers[i] = random.Next(1, 100);
+}
+
+// Находим сумму элементов на нечетных позициях
+int sum = 0;
+for (int i = 1; i < length; i += 2)
+{
+sum += numbers[i];
+}
+
+Console.WriteLine("Сгенерированный массив:");
+for (int i = 0; i < length; i++)
+{
+Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\nСумма элементов на нечетных позициях: " + sum);
+
+Console.ReadLine();
+}
